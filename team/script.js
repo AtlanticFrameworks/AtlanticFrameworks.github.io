078f6f6cec0a0b10bf59233303d948e34186e901
AtlanticFrameworks <atlanticframeworks@gmail.com>
2025-05-05 19:18:05 +0200

    Update script.js

--- team/script.js
+++ team/script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".dropdown-btn");
-  
-    buttons.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        const content = btn.nextElementSibling;
-        const isVisible = content.style.display === "block";
-        content.style.display = isVisible ? "none" : "block";
-      });
-    });
-  });
-  
+
